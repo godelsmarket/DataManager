16f9ba388391407cb15ec56b5015ffa84bdac3c4
Godelsmarket <godelsmarket@gmail.com>
2015-08-29 17:15:29 -0700

    Initial Commit.

---
+++ DataManager/DataManager.py
@@ -0,0 +1,50 @@
+
+###
+#
+#	Pulls down and manages data.
+#
+#	Right now I just want to be able to abstract downloading a historical CSV
+#	for a single company. Currently using Google finance.
+#
+###
+
+import requests
+import csv
+
+class DataManager:
+
+	def __init__(self):
+		pass
+
+	def downloadData(self, symbol):
+		""" 
+		Return a csv object with symbol data.
+		csv object is iterable by csv.reader
+		"""
+
+		#Holds the base Google finance URL
+		base_data_url = "http://www.google.com/finance/historical?output=csv&q="
+		#Create full URL for downloading desired data
+		download_url = base_data_url + symbol
+
+		#Download
+		csv = (requests.get(download_url)).content
+		csv = csv.split() #format string to be iterable for csv.reader
+
+		return csv
+
+	def getData(self, symbol):
+		"""
+		Basic get function. Returns data.
+		"""
+		pass
+
+if __name__ == "__main__":
+
+	datamanager = DataManager()
+	spycsv = datamanager.downloadData("AAPL")
+
+	csvreader = csv.reader(spycsv)
+	for row in csvreader:
+		print row
+
--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 # DataManager
+
+## Adding to a project
+
+Save the "DataManager" folder to your project.
+You'll then be able to access it as shown in the example below.
+
+## Example
+
+```
+from DataManager import DataManager
+
+import csv
+
+symbol = "NFLX"
+
+dm = DataManager.DataManager()
+
+symbolCSV = dm.downloadData(symbol)
+
+csvreader = csv.reader(symbolCSV)
+for row in csvreader:
+    print row
+    #print row[1] #for specific column
+```
